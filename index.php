a25acecbd569219020c0218039fca859b57554ff
Oleg Korneev <waterpoleg@gmail.com>
2020-03-30 21:26:29 +0300

    add new slide (N5, ref to standards page) to index page

--- index.php
+++ index.php
@@ -73,6 +73,15 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="swiper-slide" style="background-image: url('images/slider/4.jpg'); background-position: center top;">
+                            <div class="container clearfix">
+                                <div class="slider-caption slider-caption-center">
+                                    <a href="standards.php"><h2 data-caption-animate="fadeInUp">Публичное обсуждение проектов профессиональных стандартов в наноиндустрии</h2></a>
+                                    <br>
+                                    <a data-caption-animate="fadeInUp" data-caption-delay="300" href="/standards.php" target="_blank" class="button button-border button-large button-rounded tright nomargin"><span>Ознакомиться</span> <i class="icon-angle-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                     <div id="slider-arrow-left"><i class="icon-angle-left"></i></div>
