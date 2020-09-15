8828bb0389bf65bb0b6d030e01db2cc2d81d2b08
waterpoleg <waterpoleg@gmail.com>
2020-09-15 16:53:19 +0300

    added new page - profession

---
+++ profession.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+
+	<?php include "includes/head-meta.php"; ?>
+	<!-- Document Title
+	============================================= -->
+	<title>Цели и задачи | ЦОК</title>
+
+</head>
+
+	<body class="stretched">
+
+		<!-- Document Wrapper
+		============================================= -->
+		<div id="wrapper" class="clearfix">
+
+			<!-- Header
+			============================================= -->
+			<header id="header" class="full-header">
+
+				<?php include "includes/primary-menu.php"; ?>
+
+			</header><!-- #header end -->
+
+			<!-- Page Title
+			============================================= -->
+			<section id="page-title" class="page-title-parallax page-title-dark" style="background-image: url('images/goals-bg.jpg'); padding: 120px 0;" data-stellar-background-ratio="0.3">
+
+				<div class="container clearfix">
+					<h1>Вход в профессию</h1>
+					<!--<span>По дороге встретил текст рукопись</span>-->
+					<ol class="breadcrumb">
+						<li><a href="index.php">Главная</a></li>
+						<li class="active">Вход в профессию</li>
+					</ol>
+				</div>
+
+			</section><!-- #page-title end -->
+
+			<!-- Content
+			============================================= -->
+			<section id="content">
+				<div class="content-wrap">
+					<div class="container clearfix">
+						<div class="col_full"> <!--postcontent nobottommargin clearfix">-->
+
+							<div class="heading-block fancy-title nobottomborder title-bottom-border">
+                            	<h4>Приглашаем стать участником проекта «Вход в профессию»</h4>
+                        	</div>
+
+                            <p>Проект «Вход в профессию» продолжается уже 2 года. В прошлом году проведены работы по внедрению инструментов НОК в процедуру государственной итоговой аттестации
+                                Воронежского государственного института. При успешном прохождении теоретической части студенты получали сертификат Совета по профессиональным квалификациям. </p>
+                            <p>В конце сентября и весь октябрь включительно будут проводиться работы по апробации трех входных квалификаций по профессиональным стандартам: </p>
+
+							<div class="container clearfix">
+								<ul>
+									<li><p style="margin-bottom: 10px">Инженер-технолог в области производства наноразмерных полупроводниковых приборов и интегральных схем;</p></li>
+									<li><p style="margin-bottom: 10px">Инженер-конструктор в области производства наногетероструктурных СВЧ-монолитных интегральных схем;</p></li>
+                                    <li><p>Инженер в области проектирования и сопровождения интегральных схем и систем на кристалле.</p></li>
+								</ul>
+							</div>
+
+                            <p>Экзамен будет проводиться на бесплатной основе. Количество мест не ограничено.</p>
+                            <p>При успешном прохождении Вам будет выдан сертификат Совета по профессиональным квалификациям. </p>
+                            <p>Приглашаем студентов для прохождения профессионального экзамена «Вход в профессию» </p>
+                            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSevXVB5xhqi5e6P_GEKi3MyA-fApzNLZD8tEaxb6quX8BwnVQ/viewform" target="_blank">Зарегистрируйтесь по ссылке</a></p>
+						</div>
+					</div>
+				</div>
+
+			</section><!-- #content end -->
+
+			<!-- Footer
+			============================================= -->
+			<?php include "includes/footer.php"; ?>
+			<!-- #footer end -->
+
+		</div><!-- #wrapper end -->
+
+		<!-- Go To Top
+		============================================= -->
+		<div id="gotoTop" class="icon-angle-up"></div>
+
+		<!-- External JavaScripts
+		============================================= -->
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/plugins.js"></script>
+
+		<!-- Footer Scripts
+		============================================= -->
+		<script type="text/javascript" src="js/functions.js"></script>
+
+	</body>
+</html>
