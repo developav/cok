1859bd99aba49bc81127624d23f5b80a4d748275
waterpoleg <waterpoleg@gmail.com>
2021-06-10 14:05:06 +0300

    add new blog record for 30-5-21

---
+++ blog-300521.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+	<?php include "includes/head-meta.php"; ?>
+	<title>Блог | ЦОК</title>
+</head>
+<body class="stretched">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+			<?php include "includes/primary-menu.php"; ?>
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+			<div class="container clearfix">
+				<!-- <h1>Блог</h1> -->
+				<ol class="breadcrumb">
+					<li><a href="index.php">Главная</a></li>
+					<li><a href="/blog.php">Блог</a></li>
+				</ol>
+			</div>
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+					<!-- Post Content
+					============================================= -->
+					<div class="single-post nobottommargin">
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h3>5 сайтов для тех, кто хочет больше узнать о проектах и достижениях в области независимой оценки квалификаций</h3>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<ul class="entry-meta clearfix">
+								<li><i class="icon-calendar3"></i> 30 Мая 2021</li>
+							</ul><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+                            <!--<div class="col_half">
+                                <img src="images/blog/full/260421-1.png" alt="Blog 260421-1">
+                            </div>-->
+
+							<!-- Entry Content
+							============================================= -->
+                            <div class="entry-content notopmargin" style="text-align: justify"><!--class="col_half col_last">-->
+
+                                <div class="col_half">
+                                    <img src="images/blog/full/300521.png" alt="Blog 300521">
+                                </div>
+
+                                <p>• Минтруда России: <a href="https://mintrud.gov.ru/ministry/programms/22">https://mintrud.gov.ru/ministry/programms/22</a><br>
+                                На сайте представлен перечень наиболее часто задаваемых вопросов и ответов по реализации Федерального закона от 3 июля 2016 г. № 238-ФЗ «О независимой оценке квалификации»</p>
+                                <p>• ВНИИ труда: <a href="https://vcot.info/standards">https://vcot.info/standards</a><br>
+                                Подробно представлен Реестр профессиональных стандартов, нормативные правовые акты, справочники, классификаторы профессий. Возможна запись на семинары, которые проводятся для разработчиков и пользователей профстандартов.</p>
+                                <p>• Совет по квалификациям в наноиндустрии: <a href="https://spknano.ru">https://spknano.ru</a><br>
+                                Совет всесторонне и подробно освещает результаты работы в части применения системы независимой оценки квалификаций в отрасли. Представлены реестры квалификаций, перечень Центров оценки квалификаций, богатая база знаний и результатов работы СПК. На сайте размещен реестр свидетельств о квалификации, актуальных на текущий момент.</p>
+                                <p>• Национальное агентство развития квалификаций: <a href="https://nark.ru">https://nark.ru</a><br>
+                                Консолидированная информация о независимой оценке квалификаций в удобном и доступном формате. Исследования и новости в области развития НСК и профессионально-общественной аккредитации.  Представлены инфографики, наглядно демонстрирующие как организовать ЦОК или пройти независимую оценку квалификации.</p>
+                                <p>• Межотраслевое объединение в наноиндустрии: <a href="https://www.monrf.ru/materials/standards/">https://www.monrf.ru/materials/standards/</a><br>
+                                Размещена информация о разработке и публичном обсуждении профессиональных стандартов, которое проходит в настоящее время, а также перечень профстандартов для ознакомления.</p>
+                                <p><br>
+                                </p>
+                                
+
+                            </div>
+                        </div><!-- .entry end -->
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<?php include "includes/footer.php"; ?>
+		<!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<script src="js/menu-hilight.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
--- blog.php
+++ blog.php
@@ -69,6 +69,25 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-300521.php"><img class="image_fade" src="images/blog/grid/300521.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <!--<h2><a href="blog-300521.php">5 сайтов для тех, кто хочет больше узнать о проектах и достижениях в области независимой оценки квалификаций</a></h2>-->
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 30/05/2021</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p>5 сайтов для тех, кто хочет больше узнать о проектах и достижениях в области независимой оценки квалификаций<a href="/blog-300521.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-260421-3.php"><img class="image_fade" src="images/blog/grid/260421-3.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
