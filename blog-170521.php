2290119daf64bf3ff606189a7aee611e0b013fb2
waterpoleg <waterpoleg@gmail.com>
2021-06-10 14:50:19 +0300

    add new blog record for 17-05-21

---
+++ blog-170521.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+	<?php include "includes/head-meta.php"; ?>
+	<title>Блог | ЦОК</title>
+</head>
+<body class="stretched">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+			<?php include "includes/primary-menu.php"; ?>
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+			<div class="container clearfix">
+				<!-- <h1>Блог</h1> -->
+				<ol class="breadcrumb">
+					<li><a href="index.php">Главная</a></li>
+					<li><a href="/blog.php">Блог</a></li>
+				</ol>
+			</div>
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+					<!-- Post Content
+					============================================= -->
+					<div class="single-post nobottommargin">
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h3>Рубрика вопрос-ответ</h3>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<ul class="entry-meta clearfix">
+								<li><i class="icon-calendar3"></i> 30 Мая 2021</li>
+							</ul><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+                            <!--<div class="col_half">
+                                <img src="images/blog/full/260421-1.png" alt="Blog 260421-1">
+                            </div>-->
+
+							<!-- Entry Content
+							============================================= -->
+                            <div class="entry-content notopmargin" style="text-align: justify"><!--class="col_half col_last">-->
+
+                                <div class="col_half">
+                                    <img src="images/blog/full/170521.png" alt="Blog 170521">
+                                </div>
+
+                                <p>Изучив предложенные вами проекты профессиональных стандартов нашёл их похожими на уже разработанные и действующие стандарты. Также хотелось бы отметить, что те трудовые функции, которые описываются профстандартом  не могут выполняться одним человеком.</p>
+                                <p>Ответ эксперта:</p>
+                                <p>Безусловно, при разработке профессиональных стандартов брались во внимание существующие стандарты в отрасли. Мы проводим актуализацию действующих ПС на базе существующих, поэтому положения действующих стандартов были дополнены и объединены в единый общий стандарт отвечающий современным требованиям.</p>
+                                <p>Важно, что каждый этап работ по ПС сопровождается независимой экспертизой, в том числе сотрудниками реальных производств. Их замечания были учтены в настоящей редакции и по мнению экспертов, обсуждаемый ПС удовлетворяет реальным требованиям к навыкам, умениям и знаниям Специалиста.</p>
+                                <p>Задача ПС не пытаться уместить все знания и умения в одном человеке. Задача ПС - корректно и максимально точно описать «потребности» лаборатории по данному направлению деятельности.</p>
+                                <p>Под каждую обобщенную трудовую функцию набирается один, а может и больше сотрудников. Каждый специализируется на своем участке работы.</p>
+                                <p>Для внесения конструктивных замечаний/предложений по усовершенствованию документа, мы проводим профессионально-общественное обсуждение.<br>
+                                Вы можете принять в нем участие.<br>
+                                Записывайтесь на экспертный вебинар, посвященный обсуждению профстандартов.<br>
+                                Мероприятие проводится при поддержке ОЭЗ ТехнополисМосква.</p>
+                                <p></p>
+                                <p></p>
+                                <p></p>
+                                <p></p>
+                                <p></p>
+                                <p></p>
+                                <p></p>
+
+                            </div>
+                        </div><!-- .entry end -->
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<?php include "includes/footer.php"; ?>
+		<!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<script src="js/menu-hilight.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
--- blog.php
+++ blog.php
@@ -88,6 +88,25 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-170521.php"><img class="image_fade" src="images/blog/grid/170521.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <h2><a href="blog-170521.php">Рубрика вопрос-ответ</a></h2>
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 17/05/2021</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p>Изучив предложенные вами проекты профессиональных стандартов нашёл их похожими на уже разработанные и действующие стандарты. Также хотелось бы отметить, что те трудовые функции, которые описываются профстандартом  не могут выполняться одним человеком.<a href="/blog-170521.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-260421-3.php"><img class="image_fade" src="images/blog/grid/260421-3.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
