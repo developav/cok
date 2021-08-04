451cd5eb16e5c088e2b6777ea5f67e5804422f07
waterpoleg <waterpoleg@gmail.com>
2021-08-04 21:17:23 +0300

    new blog record for 30-07-21

--- blog.php
+++ blog.php
@@ -50,6 +50,25 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-300721.php"><img class="image_fade" src="images/blog/grid/300721.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <h2><a href="blog-300721.php">5 аргументов в пользу применения инструментов НСК в работе HR- подразделений</a></h2>
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 30/07/2021</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p><a href="/blog-300721.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-280721.php"><img class="image_fade" src="images/blog/grid/280721.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
