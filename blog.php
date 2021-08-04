1e96029f743df2ef064deafed6360c3af3b4835c
waterpoleg <waterpoleg@gmail.com>
2021-08-04 20:57:58 +0300

    new blog record for 28-07-21

--- blog.php
+++ blog.php
@@ -50,6 +50,25 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-280721.php"><img class="image_fade" src="images/blog/grid/280721.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <h2><a href="blog-280721.php"></a></h2>
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 28/07/2021</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p>Свидетельство о квалификации – документ государственного образца, подтверждающий успешное прохождение профессионального экзамена в рамках №238-ФЗ «О независимой оценке квалификации».<a href="/blog-280721.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-190721.php"><img class="image_fade" src="images/blog/grid/190721.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
