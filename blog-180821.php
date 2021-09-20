7afb11365010d960f96d3758bf95c17d693c72ed
waterpoleg <waterpoleg@gmail.com>
2021-09-20 09:54:35 +0300

    new blog record 18 08 21

---
+++ blog-180821.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+	<?php include "includes/head-meta.php"; ?>
+	<title>Блог | ЦОК</title>
+</head>
+<body class="stretched">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+			<?php include "includes/primary-menu.php"; ?>
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+			<div class="container clearfix">
+				<!-- <h1>Блог</h1> -->
+				<ol class="breadcrumb">
+					<li><a href="index.php">Главная</a></li>
+					<li><a href="/blog.php">Блог</a></li>
+				</ol>
+			</div>
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+					<!-- Post Content
+					============================================= -->
+					<div class="single-post nobottommargin">
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h3>Как работает технический эксперт?</h3>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<ul class="entry-meta clearfix">
+								<li><i class="icon-calendar3"></i> 18 Августа 2021</li>
+							</ul><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+                            <!--<div class="col_half">
+                                <img src="images/blog/full/260421-1.png" alt="Blog 260421-1">
+                            </div>-->
+
+							<!-- Entry Content
+							============================================= -->
+                            <div class="entry-content notopmargin" style="text-align: justify"><!--class="col_half col_last">-->
+
+                                <div class="col_half">
+                                    <img src="images/blog/full/180821.png" alt="Blog 180821">
+                                </div>
+
+                                <p>ЯНОВИЧ СЕРГЕЙ ИГОРЕВИЧ, главный специалист АО «НИИМЭ» проверяет материалы экзамена на 7 уровень квалификации по Профессиональному стандарту № 168 «Специалист по проектному управлению в области разработки и постановки производства полупроводниковых приборов и систем с использованием нанотехнологий».</p>
+                                <p>Технический эксперт - это специалист, имеющий высокий статус в профессиональном сообществе, наделенный полномочиями для проведения профессионального экзамена и оценки его результата. Эксперт обладает знаниями и квалификацией в определенной области профессиональной деятельности, он должен быть аттестован в СПК в наноиндустрии </p>
+                                <p>Сергей Игоревич – является техническим экспертом более 4 лет. В 1982 окончил МИЭТ по специальности «Автоматика и электроника». Сергей Игоревич активно принимал участие при внедрении новых технологий 180 и 90 нм на Микроне. В НИРах и ОКРах, реализуемых НИИМЭ его часто привлекают как технолога-эксперта. Кроме того, он технический редактор различных научный сборников, выпускаемых НИИМЭ.</p>
+                                <p>Что же входит в задачи технического эксперта?</p>
+                                <p><i class="icon-line2-pin"></i> Разработка комплектов оценочных средств и актуализация</p>
+                                <p><i class="icon-line2-pin"></i> Проверка результаты экзаменов, в том числе практические задания</p>
+                                <p><i class="icon-line2-pin"></i> Принятие решения о допуске экзаменуемого к практической части экзамена</p>
+                                <p><i class="icon-line2-pin"></i> Присутствие на экзамене, если у экзаменуемого есть вопросы, эксперт отвечает на них</p>
+                                <p>Обучение экспертов можно пройти в <a href="https://www.facebook.com/educenterniime/" target="_blank">Учебный центр АО НИИМЭ</a> После успешного освоения программы подготовки экспертов по оценке квалификаций выдается свидетельство о повышении квалификации.</p>
+
+                            </div>
+                        </div><!-- .entry end -->
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<?php include "includes/footer.php"; ?>
+		<!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<script src="js/menu-hilight.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
--- blog.php
+++ blog.php
@@ -50,6 +50,25 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-180821.php"><img class="image_fade" src="images/blog/grid/180821.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <h2><a href="blog-180821.php">Как работает технический эксперт?</a></h2>
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 18/08/2021</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p>Технический эксперт - это специалист, имеющий высокий статус в профессиональном сообществе, наделенный полномочиями для проведения профессионального экзамена и оценки его результата<a href="/blog-180821.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-130821.php"><img class="image_fade" src="images/blog/grid/130821.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
