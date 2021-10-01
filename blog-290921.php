5efd119a9da9aedef49507c0de6f97bbe3c13fa9
waterpoleg <waterpoleg@gmail.com>
2021-10-01 13:01:43 +0300

    + blog 29 09

---
+++ blog-290921.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+	<?php include "includes/head-meta.php"; ?>
+	<title>Блог | ЦОК</title>
+</head>
+<body class="stretched">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+			<?php include "includes/primary-menu.php"; ?>
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+			<div class="container clearfix">
+				<!-- <h1>Блог</h1> -->
+				<ol class="breadcrumb">
+					<li><a href="index.php">Главная</a></li>
+					<li><a href="/blog.php">Блог</a></li>
+				</ol>
+			</div>
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+					<!-- Post Content
+					============================================= -->
+					<div class="single-post nobottommargin">
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h3>Руководитель ЦОК НИИМЭ Лилиана Поликарпова рассказала о деятельности Многофункционального центра оценки квалификации НИИ Молекулярной Электроники - НИИМЭ </h3>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<ul class="entry-meta clearfix">
+								<li><i class="icon-calendar3"></i> 29 Сентября 2021</li>
+							</ul><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+                            <!--<div class="col_half">
+                                <img src="images/blog/full/260421-1.png" alt="Blog 260421-1">
+                            </div>-->
+
+							<!-- Entry Content
+							============================================= -->
+                            <div class="entry-content notopmargin" style="text-align: justify"><!--class="col_half col_last">-->
+
+                                <div class="col_half">
+                                    <img src="images/blog/full/290921.png" alt="Blog 290921">
+                                </div>
+
+                                <p>"Мы нацелены и дальше развивать связь обучения с независимой оценкой квалификации, — отметила Лилиана Владимировна в своем
+                                    интервью корпоративной газете ГК НИИМЭ. — Слушатель может пройти независимую оценку, выявить квалификационный дефицит и выбрать
+                                    обучение, которое восполнит пробел в знаниях. Можно поступить иначе — сначала пройти обучение, а затем сдать профессиональный
+                                    экзамен. В обоих случаях при успешном окончании учебы и сдаче экзамена учащийся не только получает документ об образовании
+                                    установленного образца, но и подтверждает свой уровень квалификации"</p>
+                                <p>Полный текст читайте на сайте <a href="https://www.facebook.com/educenterniime/?__cft__[0]=AZUC3NC2L7kYisak4HybE82vcNX8km_PJ9zitea-xE7urEt3fvsX5nMUt4VZe6i9vhdWZTHH4HuyV8aMNNt0wLktdBsueJ6eAft8ZKWptWas5YFEV9PiyBIIV_GFgjZnHhA-p2YHOZuX_Pg6J-d1sSy1kKV-LnOP_4U82juc11PQ7PcVlehsusxvMkotR3mKBDA&__tn__=kK-R" target="_blank">Учебный центр АО НИИМЭ</a></p>
+
+                            </div>
+                        </div><!-- .entry end -->
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<?php include "includes/footer.php"; ?>
+		<!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<script src="js/menu-hilight.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
--- blog.php
+++ blog.php
@@ -50,6 +50,25 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-290921.php"><img class="image_fade" src="images/blog/grid/290921.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <h2><a href="blog-290921.php">Руководитель ЦОК НИИМЭ Лилиана Поликарпова рассказала о деятельности Многофункционального центра оценки квалификации НИИ Молекулярной Электроники - НИИМЭ</a></h2>
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 29/09/2021</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p> <a href="/blog-290921.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-280821.php"><img class="image_fade" src="images/blog/grid/280821.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
