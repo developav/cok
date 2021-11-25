f2df1847c7ac71870deea8b087df4ed56807acaa
waterpoleg <waterpoleg@gmail.com>
2021-11-25 11:52:28 +0300

    new blog record 191121

---
+++ blog-191121.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+	<?php include "includes/head-meta.php"; ?>
+	<title>Блог | ЦОК</title>
+</head>
+<body class="stretched">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+			<?php include "includes/primary-menu.php"; ?>
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+			<div class="container clearfix">
+				<!-- <h1>Блог</h1> -->
+				<ol class="breadcrumb">
+					<li><a href="index.php">Главная</a></li>
+					<li><a href="/blog.php">Блог</a></li>
+				</ol>
+			</div>
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+					<!-- Post Content
+					============================================= -->
+					<div class="single-post nobottommargin">
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h3></h3>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<ul class="entry-meta clearfix">
+								<li><i class="icon-calendar3"></i> 19 Нобяря 2021</li>
+							</ul><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+                            <!--<div class="col_half">
+                                <img src="images/blog/full/260421-1.png" alt="Blog 260421-1">
+                            </div>-->
+
+							<!-- Entry Content
+							============================================= -->
+                            <div class="entry-content notopmargin" style="text-align: justify"><!--class="col_half col_last">-->
+
+                                <div class="col_half">
+                                    <img src="images/blog/full/191121.png" alt="Blog 191121">
+                                </div>
+
+                                <p>Генеральный директор Национального агентства развития квалификаций Артем Шадрин
+                                    обозначил основной концепт в работе национальной системы квалификаций:
+                                    "Главная наша задача – это преодоление разрыва между требованиями рынка труда,
+                                    которые меняются все быстрее и быстрее, и системой подготовки кадров. Для этого
+                                    мы ориентируемся на видение развития рынка труда, имеющееся у эйчаров ведущих
+                                    российских компаний. И в рамках советов по профессиональным квалификациям,
+                                    созданных в основных отраслях экономики, предложения транслируются в содержание
+                                    профессиональных стандартов, механизмы независимой оценки квалификаций. Все это
+                                    влияет на требования к результатам программ профобразования. </p>
+
+                            </div>
+                        </div><!-- .entry end -->
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<?php include "includes/footer.php"; ?>
+		<!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<script src="js/menu-hilight.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
--- blog.php
+++ blog.php
@@ -50,6 +50,27 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-191121.php"><img class="image_fade" src="images/blog/grid/191121.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <h2><a href="blog-191121.php"></a></h2>
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 19/11/2021</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p>Генеральный директор Национального агентства развития квалификаций
+                                            Артем Шадрин обозначил основной концепт в работе национальной системы
+                                            квалификаций<a href="/blog-191121.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-151121.php"><img class="image_fade" src="images/blog/grid/151121.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
