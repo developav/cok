ed532843f3a80bb9c4ca783d8f026d5aeeac3bd5
waterpoleg <waterpoleg@gmail.com>
2021-11-25 11:42:51 +0300

    new blog record 151121

---
+++ blog-151121.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+	<?php include "includes/head-meta.php"; ?>
+	<title>Блог | ЦОК</title>
+</head>
+<body class="stretched">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+			<?php include "includes/primary-menu.php"; ?>
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+			<div class="container clearfix">
+				<!-- <h1>Блог</h1> -->
+				<ol class="breadcrumb">
+					<li><a href="index.php">Главная</a></li>
+					<li><a href="/blog.php">Блог</a></li>
+				</ol>
+			</div>
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+					<!-- Post Content
+					============================================= -->
+					<div class="single-post nobottommargin">
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h3></h3>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<ul class="entry-meta clearfix">
+								<li><i class="icon-calendar3"></i> 15 Нобяря 2021</li>
+							</ul><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+                            <!--<div class="col_half">
+                                <img src="images/blog/full/260421-1.png" alt="Blog 260421-1">
+                            </div>-->
+
+							<!-- Entry Content
+							============================================= -->
+                            <div class="entry-content notopmargin" style="text-align: justify"><!--class="col_half col_last">-->
+
+                                <div class="col_half">
+                                    <img src="images/blog/full/151121.png" alt="Blog 151121">
+                                </div>
+
+                                <p>«Национальная система квалификации, которая строится на федеральном уровне является
+                                    базовым, основным и идеальным инструментом для того, чтобы устранить разрывы между
+                                    реальными запросами рынка труда, потребностями работодателей и тем, как реагирует
+                                    на эти потребности и запросы система подготовки кадров и система высшего
+                                    образования»</p>
+                                <p>Ольга Алексеевна также подчеркивает значимость для СПК расширения присутствия и
+                                    развитие взаимодействия с разными субъектами Воронежской области – вузами,
+                                    работодателями, научным сообществом.</p>
+                                <p>Напомним, что 21 октября в Воронеже в рамках регионального этап проекта
+                                    «ОК!НАНО-тур» были подписаны соглашения с вузами о ведении совместной деятельности
+                                    Сторон в рамках развития Национальной системы квалификаций (НСК) и внедрения
+                                    независимой оценки квалификации (НОК). </p>
+                                <p>«Развитие партнерских связей в регионе является приоритетным направлением
+                                    деятельности СПК в наноиндустрии, поэтому мы являемся подписантами и участниками
+                                    Соглашения. Надеюсь, Соглашение станет основой гармоничного и активного развития
+                                    независимой оценки квалификации в Воронежской области и поможет вовлечь в систему
+                                    оценки квалификации высшие учебные заведения, работодателей и специалистов,
+                                    которые присутствуют на рынке труда», – отметила Ольга Крюкова</p>
+
+                            </div>
+                        </div><!-- .entry end -->
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<?php include "includes/footer.php"; ?>
+		<!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<script src="js/menu-hilight.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
--- blog.php
+++ blog.php
@@ -50,6 +50,25 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-151121.php"><img class="image_fade" src="images/blog/grid/151121.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <h2><a href="blog-151121.php"></a></h2>
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 15/11/2021</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p>«Национальная система квалификации, которая строится на федеральном уровне является базовым, основным и идеальным инструментом для того, чтобы устранить разрывы между реальными запросами рынка труда, потребностями работодателей и тем, как реагирует на эти потребности и запросы система подготовки кадров и система высшего образования»<a href="/blog-151121.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-290921.php"><img class="image_fade" src="images/blog/grid/290921.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
