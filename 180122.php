2759065c71813d661b61e013986165f2f11d8a95
waterpoleg <waterpoleg@gmail.com>
2022-01-24 13:32:11 +0300

    add news 180122

---
+++ 180122.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+
+    <?php include "includes/head-meta.php"; ?>
+
+    <title>Новости | ЦОК</title>
+
+</head>
+
+<body class="stretched">
+
+<!-- Document Wrapper
+============================================= -->
+<div id="wrapper" class="clearfix">
+
+    <!-- Header
+    ============================================= -->
+    <header id="header" class="full-header">
+
+        <?php include "includes/primary-menu.php"; ?>
+
+    </header><!-- #header end -->
+
+    <!-- Page Title
+    ============================================= -->
+    <section id="page-title">
+
+        <div class="container clearfix">
+            <!-- <h1>Новости</h1> -->
+            <ol class="breadcrumb">
+                <li><a href="index.php">Главная</a></li>
+                <li><a href="index.php#cok_news">Новости</a></li>
+            </ol>
+        </div>
+
+    </section><!-- #page-title end -->
+
+    <!-- Content
+    ============================================= -->
+    <section id="content">
+
+        <div class="content-wrap">
+
+            <div class="container clearfix">
+
+                <!-- Post Content
+                ============================================= -->
+                <div class="single-post nobottommargin">
+
+                    <!-- Single Post
+                    ============================================= -->
+                    <div class="entry clearfix">
+
+                        <!-- Entry Title
+                        ============================================= -->
+                        <div class="entry-title">
+                            <h3>Привлечение к работе молодых специалистов в настоящее время является одной из ключевых задач рынка труда.</h3>
+                            <!--<p><i><b style="color:blueviolet">материал дополняется</b></i></p>-->
+                        </div><!-- .entry-title end -->
+
+                        <!-- Entry Meta
+                        ============================================= -->
+                        <ul class="entry-meta clearfix">
+                            <li><i class="icon-calendar3"></i>18 Января 2022</li>
+                        </ul><!-- .entry-meta end
+
+                        <!- Entry Image
+                        ============================================= -->
+
+                        <!-- Entry Content
+                        ============================================= -->
+                        <div class="entry-content notopmargin" style="text-align: justify;">
+                            <div class="image bottommargin topmargin-sm col_half">
+                                <img src="images/news/full/181.png" alt="News 180122">
+                            </div><!-- .entry-image end -->
+                            <p>Не секрет, что передовые компании отрасли, например, НИИ Молекулярной Электроники - НИИМЭ, уже длительное время самостоятельно готовят и доучивают студентов и выпускников своими силами и в коллаборации с учебными заведениями.</p>
+                            <p>Проведение профэкзамена по "входным" #квалификациям позволит выстроить свою карьерную траекторию специалиста ещё в начале пути, открывая перед ним возможности развития и раскрытия профессионального потенциала.</p>
+                            <p>Работодатель же, привлекая к работе молодых специалистов после прохождения #профэкзамена, получает информацию об имеющихся квалификационных дефицитах и может предусмотреть обучающие мероприятия уже в процессе адаптации новичка, тем самым существенно сократить процесс "доучивания".</p>
+                            <p>14 декабря 2021 года Правительством Российской Федерации принята программа содействия занятости молодежи на период до 2030 года.</p>
+                            <p>Содержание программы по ссылке </p>
+                            <p><a href="http://static.government.ru/media/files/rPqTKcZXAGKm9YF3xVcoANoHZSUnnpE6.pdf" target="_blank">http://static.government.ru/media/files/rPqTKcZXAGKm9YF3xVcoANoHZSUnnpE6.pdf</a></p>
+                        </div>
+                    </div>
+                </div><!-- .entry end -->
+            </div>
+        </div>
+</div>
+</section><!-- #content end -->
+
+<!-- Footer
+============================================= -->
+<?php include "includes/footer.php"; ?>
+<!-- #footer end -->
+
+</div><!-- #wrapper end -->
+
+<!-- Go To Top
+============================================= -->
+<div id="gotoTop" class="icon-angle-up"></div>
+
+<!-- External JavaScripts
+============================================= -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/plugins.js"></script>
+
+<script src="js/menu-hilight.js"></script>
+
+<!-- Footer Scripts
+============================================= -->
+<script type="text/javascript" src="js/functions.js"></script>
+
+<script type="text/javascript">
+
+</script>
+
+</body>
+</html>
