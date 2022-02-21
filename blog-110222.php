74d990ba2b00c1268a1e77661d82bea92e762c4f
waterpoleg <waterpoleg@gmail.com>
2022-02-21 18:26:29 +0300

    add blog 11 02 22

---
+++ blog-110222.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+	<?php include "includes/head-meta.php"; ?>
+	<title>Блог | ЦОК</title>
+</head>
+<body class="stretched">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+			<?php include "includes/primary-menu.php"; ?>
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+			<div class="container clearfix">
+				<!-- <h1>Блог</h1> -->
+				<ol class="breadcrumb">
+					<li><a href="index.php">Главная</a></li>
+					<li><a href="/blog.php">Блог</a></li>
+				</ol>
+			</div>
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+					<!-- Post Content
+					============================================= -->
+					<div class="single-post nobottommargin">
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h3></h3>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<ul class="entry-meta clearfix">
+								<li><i class="icon-calendar3"></i> 11 Февраля 2022</li>
+							</ul><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+                            <!--<div class="col_half">
+                                <img src="images/blog/full/260421-1.png" alt="Blog 260421-1">
+                            </div>-->
+
+							<!-- Entry Content
+							============================================= -->
+                            <div class="entry-content notopmargin" style="text-align: justify"><!--class="col_half col_last">-->
+
+                                <div class="col_half">
+                                    <img src="images/blog/full/110222.png" alt="Blog 110222">
+                                </div>
+                                <p>Эксперты ЦОК «НИИМЭ» - это специалисты в области наноиндустрии, обладающие высокой квалификацией и опытом работы в отрасли.</p>
+                                <p>Представляем интервью Кравцова Александра, главного конструктора НИИМЭ, который также является техническим экспертом ЦОК.</p>
+                                <p>Читайте подробнее по ссылке:</p>
+                                <a href="https://www.niime.ru/upload/iblock/e95/e953736cc953c9ba46a236b00126b8f5.pdf?fbclid=IwAR1uCDbDVXldJ-ntGyEqlsySvR7__cBb6ww3ND_mAdTP4ic4RvK5JiWeU3w" target="_blank">
+                                        https://www.niime.ru/upload/iblock/e95/e953736cc953c9ba46a236b00126b8f5.pdf?fbclid=IwAR1uCDbDVXldJ-ntGyEqlsySvR7__cBb6ww3ND_mAdTP4ic4RvK5JiWeU3w</a></p>
+                                <p></p>
+                            </div>
+                        </div><!-- .entry end -->
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<?php include "includes/footer.php"; ?>
+		<!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<script src="js/menu-hilight.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+	<script type="text/javascript">
+
+	</script>
+</body>
+</html>
--- blog.php
+++ blog.php
@@ -50,6 +50,25 @@
                         <div id="posts" class="col_full small-thumbs">
                             <div class="entry clearfix">
                                 <div class="entry-image col_one_forth">
+                                    <a href="/blog-110222.php"><img class="image_fade" src="images/blog/grid/110222.png" alt="Image"></a>
+                                </div>
+                                <div class="entry-c col_three_forth col_last">
+                                    <div class="entry-title">
+                                        <h2><a href="blog-110222.php"></a></h2>
+                                    </div>
+                                    <ul class="entry-meta clearfix" style="height: 30px">
+                                        <li><i class="icon-calendar3"></i> 11/02/2022</li>
+                                    </ul>
+                                    <div class="entry-content col_last">
+                                        <p>Эксперты ЦОК «НИИМЭ» - это специалисты в области наноиндустрии, обладающие высокой квалификацией и опытом работы в отрасли.<a href="/blog-110222.php" class="more-link"> (Дальше...)</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- #posts end -->
+
+                        <div id="posts" class="col_full small-thumbs">
+                            <div class="entry clearfix">
+                                <div class="entry-image col_one_forth">
                                     <a href="/blog-030222.php"><img class="image_fade" src="images/blog/grid/030222.png" alt="Image"></a>
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
