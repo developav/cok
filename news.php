fe8705bafd88782e88b46b146611c4635294cbf8
waterpoleg <waterpoleg@gmail.com>
2022-11-09 16:17:00 +0300

    fix date

--- news.php
+++ news.php
@@ -130,7 +130,7 @@
                                 </div>
                                 <div class="entry-c col_three_forth col_last">
                                     <div class="entry-title">
-                                        <h2><a href="290822.php">Продолжается всеросийский конкурс профессионального мастерства<br> "Лучший по професии"</a></h2>
+                                        <h2><a href="310822.php">Продолжается всеросийский конкурс профессионального мастерства<br> "Лучший по професии"</a></h2>
                                     </div>
                                     <ul class="entry-meta clearfix" style="height: 30px">
                                         <li><i class="icon-calendar3"></i> 31/8/2022</li>
