7414e27478fe1e1e63a8024bd74f7b284e3d9f9d
Павлов Дмитрий Евгеньевич <dpavlov@niime.ru>
2022-12-27 13:54:36 +0300

    add 4 news at november

---
+++ 3011222.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+
+    <?php include "includes/head-meta.php"; ?>
+
+    <title>Новости | ЦОК</title>
+
+</head>
+
+<body class="stretched">
+
+<!-- Document Wrapper
+============================================= -->
+<div id="wrapper" class="clearfix">
+
+    <!-- Header
+    ============================================= -->
+    <header id="header" class="full-header">
+
+        <?php include "includes/primary-menu.php"; ?>
+
+    </header><!-- #header end -->
+
+    <!-- Page Title
+    ============================================= -->
+    <section id="page-title">
+
+        <div class="container clearfix">
+            <!-- <h1>Новости</h1> -->
+            <ol class="breadcrumb">
+                <li><a href="index.php">Главная</a></li>
+                <li><a href="index.php#cok_news">Новости</a></li>
+            </ol>
+        </div>
+
+    </section><!-- #page-title end -->
+
+    <!-- Content
+    ============================================= -->
+    <section id="content">
+
+        <div class="content-wrap">
+
+            <div class="container clearfix">
+
+                <!-- Post Content
+                ============================================= -->
+                <div class="single-post nobottommargin">
+
+                    <!-- Single Post
+                    ============================================= -->
+                    <div class="entry clearfix">
+
+                        <!-- Entry Title
+                        ============================================= -->
+                        <div class="entry-title">
+                            <h3>ЦОК «НИИМЭ» разработал комплекты оценочных средств для рабочих специальностей в микроэлектронике</h3>
+                            <!--<p><i><b style="color:blueviolet">материал дополняется</b></i></p>-->
+                        </div><!-- .entry-title end -->
+
+                        <!-- Entry Meta
+                        ============================================= -->
+                        <ul class="entry-meta clearfix">
+                            <li><i class="icon-calendar3"></i> 30 ноября 2022</li>
+                        </ul><!-- .entry-meta end
+
+                        <!- Entry Image
+                        ============================================= -->
+
+                        <!-- Entry Content
+                        ============================================= -->
+                        <div class="entry-content notopmargin" style="text-align: justify;">
+                            <div class="image bottommargin topmargin-sm col_half" style="font-size: small;">
+                                <img src="images/news/full/234.png" alt="News 3011222">
+                            </div><!-- .entry-image end -->
+
+                            <p>В ноябре 2022 года ЦОК АО "НИИМЭ" завершил разработку КОС для Рабочих специальностей производственных участков изготовления изделий микроэлектроники. Комплекты оценочных средств ориентированы на ключевых сотрудников, участвующих в производственном процессе изготовления полупроводниковых изделий:</p>
+                            <p>- оператор технологических процессов производства приборов квантовой электроники и фотоники (3 уровень квалификации).</p>
+                            <p>- наладчик оборудования для производства приборов квантовой электроники и фотоники (4 уровень квалификации).</p>
+                            <p>Оценочные средства утверждены на заседании Совета по профессиональным квалификациям в наноиндустрии.</p>
+                            <p>21 декабря 2022 года на экзамен были приглашены 5 наладчиков оборудования и 10 операторов производственных подразделений завода «Микрон».</p>
+                            <p>Участники процесса уложились в временные рамки процедуры проведения профессионального экзамена. Слушатели отметили, что экзамен мотивирует их и далее изучать, и познавать особенности своей профессиональной деятельности, понимать теоретические основы, совершенствовать практические приемы выполнения технологических операций, строить свою профессиональную карьеру.</p>
+                            <p>В реализации планов соискателей реальную помощь окажет анализ результатов проведенного профессионального экзамена, определение квалификационных дефицитов для каждого соискателя.</p>
+                        </div>
+                    </div>
+                </div><!-- .entry end -->
+            </div>
+        </div>
+</div>
+</section><!-- #content end -->
+
+<!-- Footer
+============================================= -->
+<?php include "includes/footer.php"; ?>
+<!-- #footer end -->
+
+</div><!-- #wrapper end -->
+
+<!-- Go To Top
+============================================= -->
+<div id="gotoTop" class="icon-angle-up"></div>
+
+<!-- External JavaScripts
+============================================= -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/plugins.js"></script>
+
+<script src="js/menu-hilight.js"></script>
+
+<!-- Footer Scripts
+============================================= -->
+<script type="text/javascript" src="js/functions.js"></script>
+
+<script type="text/javascript">
+
+</script>
+
+</body>
+</html>
