8db31ce8fbd03e4e3e108cc52dd3cceeea2f489f
Павлов Дмитрий Евгеньевич <dpavlov@niime.ru>
2023-01-12 13:59:22 +0300

    change sendemail file

--- sendmail.php
+++ sendmail.php
@@ -11,33 +11,32 @@
 	$mail->CharSet = 'UTF-8';
 	$mail->setLanguage('ru', 'phpmailer/language/');
 	$mail->isHTML(true);
-    $mail->isSMTP();    
-	$mail->SMTPDebug = 2;                                        //Send using SMTP
-    $mail->Host       = 'smtp.timeweb.ru';                     //Set the SMTP server to send through
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'mail.nic.ru';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'mail@cb90059.tmweb.ru';                     //SMTP username
-    $mail->Password   = 'Frontdeveloper1!';                               //SMTP password
+    $mail->Username   = 'info@cok-niime.nichost.ru';                     //SMTP username
+    $mail->Password   = '09October2018';                               //SMTP password
     $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 	// От кого письмо
-	$mail->setFrom('mail@cb90059.tmweb.ru');
+	$mail->setFrom('info@cok-niime.nichost.ru');
 	// Кому отправить
-	$mail->addAddress('developavlov@yandex.ru');
+	$mail->addAddress('dpavlov@niime.ru');
 	// Тема письма
-	$mail->Subject = 'Привет! Это "ProspectSchool"';
+	$mail->Subject = 'ЦОК "НИИМЭ"';
 
 	// Тело письма
-	$body = '<h1>Встречайте супер письмо!</h1>';
+	$body = '<h1>Письмо с сайта ЦОК "НИИМЭ"!</h1>';
 
 	if(trim(!empty($_POST['name']))){
 		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
 	}
-	if(trim(!empty($_POST['phone']))){
-		$body.='<p><strong>Телефон:</strong> '.$_POST['phone'].'</p>';
-	}
 	if(trim(!empty($_POST['email']))){
 		$body.='<p><strong>Email:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Message:</strong> '.$_POST['message'].'</p>';
 	}
 
 	$mail->Body = $body;
